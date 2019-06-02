449f796e218ea88f13cb775419f871639834d327
Администратор <@WIN-G54R6C1QK3D>
2019-06-02 22:56:50 +0300

    Database work properly

--- WpfApp1/Database.xaml.cs
+++ WpfApp1/Database.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using BL;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+using System.Text.RegularExpressions;
 
 namespace WpfApp1
 {
@@ -20,17 +24,81 @@ namespace WpfApp1
     /// </summary>
     public partial class Database : Window
     {
+        string connectionString;
+        SqlDataAdapter adapter;
+        DataTable phonesTable;
+
         private InputData _inputData;
         public Database(out InputData inputData)
         {
             InitializeComponent();
+           // connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
             _inputData = new InputData();
             inputData = _inputData;
         }
-
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            e.Handled = Regex.IsMatch(e.Text, "[^0-9]+");
+        }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            String selectSQL = "Select * FROM All_Data";
+            Select(selectSQL,0,1);
+        }
+        private void Button_Before_Click(object sender, RoutedEventArgs e)
+        {
+            String selectSQL = "Select * FROM Inputed_Values";
+            Select(selectSQL,2,3);
+        }
+        public void Select(String selectSQL,int iX,int iy) // функция подключения к базе данных и обработка запросов
+        {
+         //   String selectSQL = "Select * FROM All_Data";
+            DataTable dataTable = new DataTable("dataBase");                // создаём таблицу в приложении
+                                                                            // подключаемся к базе данных
+            connectionString = "Data Source=WIN-G54R6C1QK3D;Initial Catalog=CalcResult;Integrated Security=True";
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();                                           // открываем базу данных
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();          // создаём команду
+            sqlCommand.CommandText = selectSQL;                             // присваиваем команде текст
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand); // создаём обработчик
+            sqlDataAdapter.Fill(dataTable);                                 // возращаем таблицу с результатом
 
+            _inputData.NOPTS = dataTable.Rows.Count;
+            _inputData.X = new float[_inputData.NOPTS];
+            _inputData.Y = new float[_inputData.NOPTS];
+
+            for (int i = 0; i < dataTable.Rows.Count; i++) { // перебираем данные
+                _inputData.X[i] = (float)Convert.ToDouble(dataTable.Rows[i][iX]);
+                _inputData.Y[i] = (float)Convert.ToDouble(dataTable.Rows[i][iy]);
+               
+            }
+           
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (Convert.ToInt32(DegreeDB.Text) > 1 && Convert.ToInt32(DegreeDB.Text) < 10)
+                {
+                    _inputData.DEGREE = Convert.ToInt32(DegreeDB.Text);
+                }
+                else
+                {
+                    DegreeDB.Text = "";
+                    ErrorDegreeDB.Text = "Value should be grater then 1 and less then 10";
+                }
+
+                DialogResult = true;
+                this.Close();
+            }
+            catch
+            {
+                MessageBox.Show("You entered some data in wrong format, all inputed values should be digits");
+                //TextBoxNumberElements.Text = "";
+                // TextBoxDegree.Text = "";
+            }
         }
     }
 }
--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -50,17 +52,19 @@ namespace WpfApp1
             Approximation_Calculation.Calculation(_inputData.DEGREE, _inputData.NOPTS, _inputData.X, _inputData.Y, _inputData.COEF);
             
             Info.Text=string.Format("{0}", "ORDER    COEFFICIENT\n");
-            for (int i = 0; i < _inputData.DEGREE + 1; i++)
+            if (_inputData.NOPTS > 0)
             {
-                Info.Text += string.Format("{0}      {1}, {2}", i, _inputData.COEF[i], '\n');
-            }
-            Info.Text += string.Format("{0}", "Y Values\n");
-            for (int i = 0; i < _inputData.NOPTS; i++)
-            {               
-                Info.Text += string.Format("{0}     {1}, {2}", i, _inputData.Y[i], '\n');
-            }
-            
+                for (int i = 0; i < _inputData.DEGREE + 1; i++)
+                {
+                    Info.Text += string.Format("{0}      {1}, {2}", i, _inputData.COEF[i], '\n');
+                }
+                Info.Text += string.Format("{0}", "Y Values\n");
+                for (int i = 0; i < _inputData.NOPTS; i++)
+                {
+                    Info.Text += string.Format("{0}     {1}, {2}", i, _inputData.Y[i], '\n');
+                }
 
+            }
 
         }
 
@@ -90,6 +94,25 @@ namespace WpfApp1
                 firstCalc();
                 CBError.Text = "";
             }
+        }
+        private void Button_Add_Click(object sender, RoutedEventArgs e)
+        {
+            //string TextOfCommand = "Update Inputed_Values set Date='"+DateTime.Now.ToString("MM/dd/yyyy")+"', Time='"+ DateTime.Now.ToString("hh:mm tt")+"', CB_input ='"+ (float)Convert.ToDouble(TextBoxCB.Text) + "', HRV_calc='"+(float)Convert.ToDouble(ResultHRV.Text)+"' ";
+            DataTable dataTable = new DataTable("dataBase");                // создаём таблицу в приложении
+            //                                                                // подключаемся к базе данных
+            string TextOfCommand = "Insert Into Inputed_Values ( Date, Time,CB_input,HRV_calc) VALUES('" + DateTime.Now.ToString("MM/dd/yyyy") + "', '"
+                    + DateTime.Now.ToString("hh:mm tt") + "', '" + (float)Convert.ToDouble(TextBoxCB.Text)
+                    + "', ' " + (float)Convert.ToDouble(ResultHRV.Text) + " ') ";
+
+            string connectionString = "Data Source=WIN-G54R6C1QK3D;Initial Catalog=CalcResult;Integrated Security=True";
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();                                           // открываем базу данных
+            
+            SqlCommand updateCommand = new SqlCommand(TextOfCommand, sqlConnection);
+            
+            updateCommand.ExecuteNonQuery();
+
+
         }
     }
 }
